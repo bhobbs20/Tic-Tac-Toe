69d1c21568e191fb1f74f1d0b20530270f60030a
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-10 11:01:42 -0500

    fixed css and html

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,8 +1,13 @@
-var arrayO = document.getElementsByClassName("O");
-var arrayX = document.getElementsByClassName("X");
-for (var i=0; i<arrayO.length; i++) {
-    arrayO[i].style.transform = "translateY(-100%)";
-}
-for (var i=0; i<arrayX.length; i++) {
-    arrayX[i].style.transform = "translateY(100%)";
-}
+
+
+
+
+
+// var arrayO = document.getElementsByClassName("O");
+// var arrayX = document.getElementsByClassName("X");
+// for (var i=0; i<arrayO.length; i++) {
+//     arrayO[i].style.transform = "translateY(-100%)";
+// }
+// for (var i=0; i<arrayX.length; i++) {
+//     arrayX[i].style.transform = "translateY(100%)";
+// }
